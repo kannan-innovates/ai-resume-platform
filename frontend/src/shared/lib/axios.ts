5920bf869b500a5239cf7afcdc98db6a1bcc4525
Kannan <kannans02001@gmail.com>
2026-02-22 14:07:54 +0530

    fix: for local and production

--- frontend/src/shared/lib/axios.ts
+++ frontend/src/shared/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL:'https://skillmount-backend.onrender.com/api',
+  baseURL:import.meta.env.VITE_API_URL || 'http://localhost:5002/api',
   timeout: 30000,
 })
 
